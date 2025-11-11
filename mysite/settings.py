15234ba55e359faf66ea401eb332708faafcb0c0
amirghadirii <amirghadiri.j@gmail.com>
2025-11-11 17:36:12 +0330

    create blog app
    website preparation and blog application create

--- mysite/settings.py
+++ mysite/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'Website.apps.WebsiteConfig'
+    'Website.apps.WebsiteConfig',
+    'blog'
 ]
 
 MIDDLEWARE = [
